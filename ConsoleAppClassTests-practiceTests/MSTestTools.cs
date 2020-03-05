34e7b78915d5b2782a934e6f4bbfdfbe77be64c1
littlehorseboy <littlehorseboy@gmail.com>
2020-03-05 22:13:07 +0800

    feat: AreSame, Inconclusive, IsInstanceOfType, IsNotInstanceOfType, IsNotNull, IsNull, ThrowsException

--- ConsoleAppClassTests-practiceTests/MSTestTools.cs
+++ ConsoleAppClassTests-practiceTests/MSTestTools.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ConsoleAppClassTests_practiceTests
 {
@@ -14,7 +15,7 @@ namespace ConsoleAppClassTests_practiceTests
             Assert.AreEqual(1, 1);
             Assert.AreEqual(true, true);
             Assert.AreEqual("string", "string");
-            Assert.AreEqual(1.0, 1.0f);
+            Assert.AreEqual(1.0, 1.0f); // 此為特殊的相等情況，但如果是用 (1.1, 1.1f) 就會 fail
             Assert.AreEqual(1, 1.0m);
         }
 
@@ -32,25 +33,13 @@ namespace ConsoleAppClassTests_practiceTests
         }
 
         [TestMethod()]
-        public void IsTrue()
+        public void AreSame()
         {
-            Assert.IsTrue(1 == 1);
-            Assert.IsTrue(true);
+            int[] array = new[] { 1, 2 };
+            Assert.AreSame(array, array);
         }
 
-        [TestMethod()]
-        public void IsNotNull()
-        {
-            Assert.IsNotNull(1 == 1);
-            Assert.IsNotNull(false);
-        }
-
-        [TestMethod()]
-        public void IsFalse()
-        {
-            Assert.IsFalse(1 == 2);
-            Assert.IsFalse(false);
-        }
+        // Assert.Equals(1, 1);
 
         [TestMethod()]
         public void Fail()
@@ -63,6 +52,60 @@ namespace ConsoleAppClassTests_practiceTests
             {
                 Assert.AreEqual(1, 1);
             }
+        }
+
+        [TestMethod()]
+        public void Inconclusive()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod()]
+        public void IsFalse()
+        {
+            Assert.IsFalse(1 == 2);
+            Assert.IsFalse(false);
+        }
+
+        [TestMethod()]
+        public void IsInstanceOfType()
+        {
+            Assert.IsInstanceOfType(1, typeof(int));
+        }
+
+        [TestMethod()]
+        public void IsNotInstanceOfType()
+        {
+            Assert.IsNotInstanceOfType(1, typeof(string));
+        }
+
+        [TestMethod()]
+        public void IsNotNull()
+        {
+            Assert.IsNotNull(1 == 1);
+            Assert.IsNotNull(false);
+        }
+
+        [TestMethod()]
+        public void IsNull()
+        {
+            Assert.IsNull(null);
+        }
+
+        [TestMethod()]
+        public void IsTrue()
+        {
+            Assert.IsTrue(1 == 1);
+            Assert.IsTrue(true);
+        }
+
+        // Assert.ReferenceEquals("1", "1");
+        // Assert.ReplaceNullChars("1");
+
+        [TestMethod()]
+        public void ThrowsException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new Regex(null));
         }
     }
 }
